2e529e549278f2f5e504262818d6f7f6dde4d790
Jeremy McGee <jeremymcgee6@gmail.com>
2019-05-24 09:54:41 -0600

    Add S3 upload

--- Deployment.ps1
+++ Deployment.ps1
@@ -1,3 +1,4 @@
 New-AWSPowerShellLambdaPackage -ScriptPath PSWindowsFact.ps1 -OutputPackage PSWindowsFact.zip
 aws cloudformation package --template-file serverless.template --s3-bucket windowsfacts-cf --output-template-file windowsfacts.template
 aws cloudformation deploy --template-file windowsfacts.template --stack-name PSWindowsFact --capabilities CAPABILITY_IAM
+Write-S3Object -BucketName windowsfacts-cf -File ..\facts\facts.json
